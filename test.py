8a7f234401f5a12d8e24305b5d5edaf47055389b
Lucas Zanotto <lucasabatizanotto@gmail.com>
2024-08-09 16:43:45 -0300

    add: Merge Strings Alternately

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-print("hello friends")
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        wordMerge = ""
+        counter = 0
+
+        if len(word1) >= len(word2):
+            counter = len(word1)
+        else:
+            counter = len(word2)
+        
+        for i in range(counter):
+            if len(word1) - 1 < i:
+                wordMerge += word2[i]
+            elif len(word2) - 1 < i:
+                wordMerge += word1[i]
+            else:
+                wordMerge += word1[i]
+                wordMerge += word2[i]
+            
+        return wordMerge
+    
