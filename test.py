f0527b7cfc24f8b09775488fb2b71bd55527ed35
Lucas Zanotto <lucasabatizanotto@gmail.com>
2024-09-01 23:52:52 -0300

    add: write-a-function

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("hello friends")
+def is_leap(year):
+    leap = False
+    if year % 4 == 0:
+        leap = True
+    if year % 100 == 0:
+        leap = False
+    if year % 400 == 0:
+        leap = True
+
+    return leap
+
+year = int(input())
+print(is_leap(year))
