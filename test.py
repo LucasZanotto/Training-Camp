eb1714929b8a384d44792a8d7c5c44e1d9763b63
Lucas Zanotto <lucasabatizanotto@gmail.com>
2024-09-02 22:23:07 -0300

    add: Kids With the Greatest Number of Candies

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("hello friends")
+class Solution:
+    def kidsWithCandies(self, candies: list[int], extraCandies: int) -> list[bool]:
+        hasGreatestCandies = 0
+        greatestList = []
+        for i in candies:
+            if hasGreatestCandies <= i:
+                hasGreatestCandies = i
+        
+        for j in candies:
+            amount = j + extraCandies
+            if hasGreatestCandies <= amount:
+                greatestList.append(True)
+            else:
+                greatestList.append(False)
+
+        return greatestList
