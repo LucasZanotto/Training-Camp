65ae5468dd0228e9ad35aff2879cf572f4b12e56
Lucas Zanotto <lucasabatizanotto@gmail.com>
2024-09-06 00:45:33 -0300

    add: solution

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("hello friends")
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']
+        vowelsList = []
+        vowelsInverted = ''
+        for i in s:
+            if vowels.__contains__(i):
+                vowelsList.append(i)
+
+        for j in s: 
+            if vowels.__contains__(j):
+                vowelsInverted += vowelsList[-1]
+                vowelsList.pop()
+                continue
+            vowelsInverted += j
+        return vowelsInverted
